33082bf1a5bf661bc7454570820e4b90c0ffbc20
Theodor Balan <teddybalan@gmail.com>
2018-04-23 22:18:31 +0200

    0.8.8

--- R/autoplot.emfrail.R
+++ R/autoplot.emfrail.R
@@ -277,7 +277,7 @@ autoplot.emfrail <- function(object,
           geom_step(aes_string(y = "cumhaz", col = shQuote("1"))) +
           geom_step(aes_string(y = "cumhaz_m", col = shQuote("2"))) +
           theme_minimal() +
-          ylab("H(t)") +
+          ylab("cumulative hazard") +
           scale_colour_manual(name = "type", values = c("#fc8d59", "#000000" ), labels = c("conditional", "marginal") )
 
         if(conf_int == "adjusted") {
